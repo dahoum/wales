a5f5ae3a4a5b4c7dd65c169fdbea188b40e92a7e
Radoslav Radev <radoslav@auxcode.com>
2020-10-06 08:55:30 +0200

    Initialize vue app

---
+++ src/components/SecondComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <HelloWorld name="Rado" :data="arr"></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from "./HelloWorld";
+
+export default {
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            arr: [1, 2, 3]
+        }
+    }
+}
+</script>
